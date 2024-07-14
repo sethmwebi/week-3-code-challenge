744b4632c34ae0754d7fb5769ae1ff7f020ba0f1
Seth Mwebi <sethmwebi27@gmail.com>
2024-07-14 18:24:24 +0300

    adds code for single movie item

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("connected");
+const movieItem = document.getElementsByClassName("movie-item")[0];
+const BASE_URL = "http://localhost:8001";
+
+const getSingleMovie = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/films/${id}`);
+
+    if (res.ok) {
+      const result = await res.json();
+      const item = document.createElement("div");
+      item.classList.add("item");
+
+      // poster
+      const img = document.createElement("img");
+      img.src = result.poster;
+      img.classList.add("poster");
+      console.log(result);
+      item.appendChild(img);
+      movieItem.appendChild(item);
+
+      const metadata = document.createElement("div");
+      metadata.classList.add("metadata");
+      item.appendChild(metadata);
+
+      // title
+      const h2 = document.createElement("h2");
+      h2.innerText = result.title;
+      metadata.appendChild(h2);
+
+      // runtime
+      const h4 = document.createElement("h4");
+      h4.innerText = `${result.runtime} minutes`;
+      metadata.appendChild(h4);
+
+      const p = document.createElement("p");
+      p.innerText = result.showtime;
+      metadata.appendChild(p);
+
+      const h3 = document.createElement("h3");
+      h3.innerText = `Available tickets: ${result.capacity - result.tickets_sold}`;
+      metadata.appendChild(h3);
+    }
+  } catch (err) {
+    console.log("Something went wrong!");
+  }
+};
+
+getSingleMovie(1);
